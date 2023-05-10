9e14aa721e9784a61f13204f2b50951b4e92220c
Einliterflasche <einliterflasche@pm.me>
2023-05-10 22:13:34 +0200

    Clean up dependencies, macro

--- pg-worm-derive/src/lib.rs
+++ pg-worm-derive/src/lib.rs
@@ -1,28 +1,28 @@
 use proc_macro::{self, TokenStream};
-use quote::{quote, format_ident};
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Data};
 
-#[proc_macro_derive(Entity)]
+#[proc_macro_derive(Model)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, data, attrs, .. } = parse_macro_input!(input);
+    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
 
-    let data_struct = match data {
-        Data::Struct(data_struct) => data_struct,
-        _ => panic!("only structs")
+    let field_idents = match data {
+        Data::Struct(data_struct) => match data_struct.fields {
+            syn::Fields::Named(fields) => 
+                fields
+                    .named
+                    .into_iter()
+                    .map(|f| f.ident.unwrap()),
+            _ => panic!("need named fields")
+        },
+        _ => unimplemented!("only structs supported")
     };
 
-    let named_fields: Vec<_> = match data_struct.fields {
-        syn::Fields::Named(named_fields) => named_fields,
-        _ => panic!("no named fields")
-    }.named.into_iter().map(|f| f.ident.unwrap()).collect();
-
-    let strs = named_fields.iter().map(|f| format_ident!("{}", f).to_string());
-
     let output = quote! {
-        impl Entity<#ident> for #ident {
-            fn from_sql(row: &tokio_postgres::Row) -> Result<#ident, tokio_postgres::Error> {
+        impl Model<#ident> for #ident {
+            fn from_row(row: &tokio_postgres::Row) -> Result<#ident, tokio_postgres::Error> {
                 Ok(#ident {
-                    #(#named_fields: row.try_get(#strs)?),*
+                    #(#field_idents: row.try_get(stringify!(#field_idents))?),*
                 })
             }
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
-//! ```
-//! #[derive(Entity)]
-//! struct Person {
-//!     id: i64,
-//!     name: String
-//! }
-pub use pg_worm_derive::Entity;
-use std::marker::PhantomData;
+use tokio_postgres::Row;
 
-use phf::Map;
-use tokio_postgres::{{types::Type}, Row};
+pub use pg_worm_derive::Model;
 
-pub struct Table<T> {
-    name: &'static str,
-    fields: &'static Map<&'static str, Type>,
-    type_: PhantomData<T>
+/// This trait allows comfortable querying.
+///
+/// # Usage
+///
+/// ```
+/// use pg_worm::Model;
+///
+/// #[derive(Model)]
+/// struct Book {
+///     id: i64,
+///     title: String
+/// }
+/// ```
+pub trait Model<T> {
+    ///
+    fn from_row(row: &Row) -> Result<T, tokio_postgres::Error>;
 }
 
-pub trait Entity<T> {
-    fn from_sql(row: &Row) -> Result<T, tokio_postgres::Error>;
+#[cfg(test)]
+mod tests {
+    use crate::Model;
+
+    #[derive(Model)]
+    struct Person {
+        id: i64,
+        name: String,
+    }
 }
